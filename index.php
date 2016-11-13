f226dd50c8f5e462aea8ccaa2f5ea4fc073e686d
Keivn <kevin@wehype.it>
2016-11-13 03:43:19 +0100

    Included functions

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 	include('./config/config.php');
 	include('./config/database.php');
+	include('./functions/functions.php');
 	include('./templates/' . $config['theme'] . '/header.php');
 
 		include('./lib/page_system.php');
