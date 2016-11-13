dcd0e13f524a02d14b59b25e5c1b4cbe217c9b66
Keivn <kevin@wehype.it>
2016-11-13 03:42:47 +0100

    Register / Login system.
    Fixed regsiter.
    Added login.
    Added logout.

--- pages/login.php
+++ pages/login.php
@@ -1,1 +1,40 @@
-<h1>Login</h1>
+<?php
+	if(isset($_POST['login'])) {
+		$account = trim($_POST['account']);
+		$password = sha1($_POST['password']);
+
+		$sql_select_account = $database->select('accounts', '*', [ 'AND' => [ 'name' => $account, 'password' => $password ] ]);
+
+		if(count($sql_select_account) == 1) {
+			$_SESSION['sess_name'] = $sql_select_account[0]['name'];
+			$_SESSION['sess_id'] = $sql_select_account[0]['id'];
+
+			header('Location: ?page=account');
+			exit;
+		}
+	}
+?>
+<ol class="breadcrumb">
+  <li><a href="./">Home</a></li>
+  <li class="active">Login</li>
+</ol>
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<h3 class="panel-title">Login</h3>
+	</div>
+	<div class="panel-body">
+	    <form action="" method="POST">
+		    <div class="input-group">
+				<span class="input-group-addon" id="basic-addon1">Account Name</span>
+				<input type="text" name="account" class="form-control" placeholder="Account name" aria-describedby="basic-addon1">
+			</div>
+
+			<div class="input-group" style="margin-top: 20px;">
+				<span class="input-group-addon" id="basic-addon1">Password</span>
+				<input type="password" name="password" class="form-control" placeholder="*********" aria-describedby="basic-addon1">
+			</div>
+			<br>
+			<input type="submit" value="Login" name="login" class="btn btn-default">
+		</form>
+	</div>
+</div>
--- pages/register.php
+++ pages/register.php
@@ -1,17 +1,29 @@
 <?php
 	if(isset($_POST['register'])) {
 		$success = false;
-		$account = $_POST['account_name'];
+		$account = trim($_POST['account_name']);
 		$password = $_POST['password'];
 		$email = $_POST['email'];
 
 		// Check if account name is already taken.
-		$db_select = $database->select('accounts', '*', [ 'name' => $account ]);
+		$db_select_account = $database->select('accounts', '*', [ 'name' => $account ]);
+		// Check if email is already in use.
+		$db_select_email = $database->select('accounts', '*', [ 'email' => $email ]);
 
 		if(empty($account) || empty($password) || empty($email)) {
 			$error = 'You have to fill in all the fields.';
-		} else if(count($db_select) == 1) {
-			$error = 'Account name already taken';
+		} else if(count($db_select_account) == 1) {
+			$error = 'Account name already taken.';
+		} else if(strlen($account) < 3) {
+			$error = 'Your account name has to be 3 characters or more.';
+		} else if(strlen($account) > 10) {
+			$error = 'Your account name has to be less then 10 characters.';
+		} else if(count($db_select_email) == 1) {
+			$error = 'Emnail is already in use.';
+		} else if(strlen($password) < 5) {
+			$error = 'Your password has to be 5 characters or more.';
+		} else if(strlen($password) > 18) {
+			$error = 'Your password has to be less then 18 characters.';
 		} else {
 			$success = true;
 		}
@@ -24,28 +36,40 @@
 				'email' => $email
 			]);
 
-			echo 'Your account has been created.';
-		} else {
-			echo $error;
+			$success = 'Your account has been created.';
 		}
 	}
 ?>
-<h2>Register account</h2>
-<form action="" method="POST">
-	<label for="account_name">
-		Account name
-	</label>
-	<input type="text" id="account_name" name="account_name">
-	<br>
-	<label for="password">
-		Password
-	</label>
-	<input type="password" id="password" name="password">
-	<br>
-	<label for="email">
-		Email
-	</label>
-	<input type="email" id="email" name="email">
-	<br>
-	<input type="submit" text="Register" name="register">
-</form>
+<ol class="breadcrumb">
+  <li><a href="./">Home</a></li>
+  <li class="active">Register</li>
+</ol>
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<h3 class="panel-title">Register</h3>
+	</div>
+	<div class="panel-body">
+		<?php 
+			if(isset($error)) { echo '<div class="text-danger" style="margin-bottom: 20px;">' . $error . '</div>'; } 
+			if(isset($success)) { echo '<div class="text-success" style="margin-bottom: 20px;">' . $success . '</div>'; }
+		?>
+	    <form action="" method="POST">
+		    <div class="input-group">
+				<span class="input-group-addon" id="basic-addon1">Account Name</span>
+				<input type="text" name="account_name" class="form-control" placeholder="Account name" aria-describedby="basic-addon1">
+			</div>
+
+			<div class="input-group" style="margin-top: 20px;">
+				<span class="input-group-addon" id="basic-addon1">Password</span>
+				<input type="password" name="password" class="form-control" placeholder="*********" aria-describedby="basic-addon1">
+			</div>
+
+			<div class="input-group" style="margin-top: 20px;">
+				<span class="input-group-addon" id="basic-addon1">Email</span>
+				<input type="text" name="email" class="form-control" placeholder="example@example.com" aria-describedby="basic-addon1">
+			</div>
+			<br>
+			<input type="submit" value="Register" name="register" class="btn btn-default">
+		</form>
+	</div>
+</div>
